79052c62f7d6e0682d3f0852295b5af40fbd1094
Raul Marco Cano <rawraul@outlook.com>
2025-01-06 22:15:21 +0000

    types

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,11 @@
 declare module '@raulcanodev/react-github-dots' {
   export interface ContributionGraphProps {
     username: string;
-    token?: string;
+    token: string;
     theme?: 'dark' | 'light' | string;
     customColorScheme?: string[];
     loadingComponent?: React.ReactNode;
+    cacheTime?: '2h' | '8h' | '1d' | false;
   }
 
   export default function ContributionGraph(props: ContributionGraphProps): JSX.Element;
